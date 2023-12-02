6f2e0213dda7783ca36103152cad04fcc270097e
kamlnskll <michael_kaminski@outlook.com>
2023-12-02 16:56:30 -0600

    Added mantine and some basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
-import './App.css';
+import { ColorInput } from '@mantine/core';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <ColorInput
+      label="Input label"
+      description="Input description"
+      placeholder="Input placeholder"
+    />
  <h1>Hello This is Working. This is the app component. Navigation goes here I believe</h1>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@mantine/core/styles.css';
+
 import App from './App';
+import { MantineProvider, createTheme } from '@mantine/core';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <MantineProvider>
     <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
