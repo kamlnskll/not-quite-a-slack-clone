44d1870dbcd3b620462df07a5fde7a9a27384d36
kamlnskll <michael_kaminski@outlook.com>
2023-12-02 17:08:46 -0600

    React Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import { ColorInput } from '@mantine/core';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <ColorInput
-      label="Input label"
-      description="Input description"
-      placeholder="Input placeholder"
-    />
- <h1>Hello This is Working. This is the app component. Navigation goes here I believe</h1>
-    </div>
+<>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home />}/>
+  <Route path='/login'  element={<Login />}/>
+  <Route path='/register'  element={<Register />}/>
+</Routes>
+</BrowserRouter>
+
+</>
+
+
+//     <div>
+//       <ColorInput
+//       label="Input label"
+//       description="Input description"
+//       placeholder="Input placeholder"
+//     />
+//  <h1>Hello This is Working. This is the app component. Navigation goes here I believe</h1>
+//     </div>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+const [mobileOpened, setMobileOpened] = useState(false);
+const [desktopOpened, setDesktopOpened] = useState(false);
+
+        return (
+            <AppShell
+              header={{ height: 60 }}
+              navbar={{
+                width: 300,
+                breakpoint: 'sm',
+                collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+              }}
+              padding="md"
+            >
+              <AppShell.Header>
+                <Group h="100%" px="md">
+ 
+            
+                </Group>
+              </AppShell.Header>
+              <AppShell.Navbar p="md">
+                Navbar
+                {Array(15)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Skeleton key={index} h={28} mt="sm" animate={false} />
+                  ))}
+              </AppShell.Navbar>
+              <AppShell.Main>Main</AppShell.Main>
+            </AppShell>
+          );
+      
 }
 
 export default Home
