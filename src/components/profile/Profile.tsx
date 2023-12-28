84edb545795ca82884d336c5660500c8cbb59f12
kamlnskll <michael_kaminski@outlook.com>
2023-12-27 21:25:13 -0600

    Basic profile card started with user id copy

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,13 +1,33 @@
-import { Avatar, Container, Paper } from '@mantine/core'
+import { Avatar, Text, Container, Group, Paper, Stack, Title, ActionIcon, Button } from '@mantine/core'
+import { IconCopy, IconCheck } from '@tabler/icons-react'
 import React from 'react'
+import { auth } from '../../firebase'
+import { useClipboard } from '@mantine/hooks';
+
 
 const Profile = () => {
+  const clipboard = useClipboard({timeout: 700})
+
   return (
     <Container>
       <Paper withBorder={true} h={'200px'}>
-      <Container ml='lg' my='50px'>
+      <Group ml='lg' my='50px'>
         <Avatar size='xl'></Avatar>
-      </Container>
+        <Stack>
+        <Title>Your name</Title>
+        <Group>
+          <Text>Your ID:</Text>
+          <Text>{auth.currentUser?.uid}</Text>
+          <Button
+          size='compact-md'
+          leftSection={clipboard.copied ? <IconCheck size={20}/> : <IconCopy size={20} />}
+          color={clipboard.copied ? 'indigo' : 'blue'}
+          onClick={() => clipboard.copy(auth.currentUser?.uid)}
+          >{clipboard.copied ? 'Copied' : 'Copy'}</Button>
+        </Group>
+        </Stack>
+        </Group>
+      
       </Paper>
 
 
