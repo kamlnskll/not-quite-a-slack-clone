b612efc96785930bb16f7074df0f42a780d765a1
kamlnskll <michael_kaminski@outlook.com>
2023-12-30 23:44:09 -0600

    added create profile func

--- src/functions/profile.ts
+++ src/functions/profile.ts
@@ -1,8 +1,34 @@
-export const getProfileData = async () => {
+import { setDoc, collection, doc, serverTimestamp } from "@firebase/firestore"
+import { auth, db } from "../firebase"
+
+const profileCollection = collection(db, 'profile')
+
+export const createProfile = async (firebaseuser_id: string) => {
+// Take Firebase auth ID and create user based on it so we can actually stop using IDs everywhere
+
+
+const profileRef = doc(profileCollection, firebaseuser_id)
+try{
+    
+await setDoc(profileRef, {
+user_id: firebaseuser_id,
+firstName: '',
+lastName: '',
+profilePic: '',
+username: '',
+createdAt: serverTimestamp()
+})
+
+
+
+} catch (err) {
+    return err
+}
+
 
 }
 
 export const editProfile = async () => {
 
-    
+
 }
